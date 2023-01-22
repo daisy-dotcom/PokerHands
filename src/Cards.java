aed2e01cb7872bf16bc11a174ecf28e21dd67b4c
daisy-dotcom <61582926+daisy-dotcom@users.noreply.github.com>
2023-01-22 23:21:00 +0300

    added methods to determine poker hand

--- src/Cards.java
+++ src/Cards.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 /*import java.util.Arrays;
 import java.util.Collections;*/
 public class Cards {
@@ -9,15 +12,34 @@ public class Cards {
 	private String cards;
 	private String[] cardArray = new String[10];
 	
-	public Cards(String cards) {
-		this.cards = cards;
+	public Cards() {
+		getCards();
+		/*this.cards = cards;*/
 		createCardArray(cards);
 		dealCards();
 		
 	}
+
+	public void getCards(){
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		String inputLine = "";
+
+		try {
+			inputLine = input.readLine();
+			cards = inputLine;
+			System.out.println(inputLine);
+		} 
+		catch (IOException e) {
+			System.out.print("Input Error!");
+			e.printStackTrace();
+		}
+
+	}
 	
 	public void createCardArray(String cards) {
 		this.cardArray = cards.split(" ");	
+		System.out.println(Arrays.asList(cardArray));
+
 	}
 	
 	public void dealCards() {
@@ -26,13 +48,15 @@ public class Cards {
 		for (int i = 0; i < arrayLength; i++) {
 			cardsToDeal[i] = cardArray[i];	
 		}
+		System.out.println(Arrays.asList(cardsToDeal));
 		
 		playerOne = new Hand(cardsToDeal);
 		
-		for (int i = 5; i < arrayLength; i++) {
+		for (int i = 5; i < (arrayLength*2); i++) {
 			cardsToDeal[i-arrayLength] = cardArray[i];
 		}
 		
+		System.out.println(Arrays.asList(cardsToDeal));
 		playerTwo = new Hand(cardsToDeal);
 		
 	}
--- src/Hand.java
+++ src/Hand.java
@@ -8,7 +8,7 @@ public class Hand{
 		put("K", 12);
 		put("Q", 11);
 		put("J", 11 );
-		put("10",10);
+		put("T", 10);
 		put("9", 9);
 		put("8", 8);
 		put("7", 7);
@@ -18,13 +18,19 @@ public class Hand{
 		put("3", 3 );
 		put("2", 2);
 	}};
-	private int cardValues []= new int [arrayLength];
-	private String cardSuits []= new String [arrayLength];
+	private Integer[] cardValues = new Integer[arrayLength];
+	private String[] cardSuits = new String [arrayLength];
+	private HashMap<Integer, Integer> cardMap = new HashMap<>();
+	private TreeSet<Integer> cardValueSet ;
+	private TreeSet<String> cardSuitsSet ;
+	private int highestCardValue;
+
 	
 	
 	public Hand(String[] cards) {
-		setCardValues(cards);
-		setCardSuits(cards);	
+		setCardValuesAndSuits(cards);
+		setCardValuesAndSuitsSet();
+		currentHand();
 	}
 	
 	public int getCardValue(String cardNumber) {
@@ -39,68 +45,235 @@ public class Hand{
 		}
 	}
 	
-	public void setCardValues(String[] cards) {
+	public void setCardValuesAndSuits(String[] cards) {
 		for (int i = 0; i < arrayLength; i++) {
-			String[] splitValue = cards[i].split("\\D");
+			String[] splitCard = cards[i].split("(?=\\D)");
 			
-			for (String value: splitValue) {
-				cardValues[i] = Integer.parseInt(value);
+			cardValues[i] = cardsAndValues.get(splitCard[0]);
+			cardSuits[i] = splitCard[1];
+
+			setCardMap(cardValues[i]);
+
+			/*System.out.println(cardValues[i]);
+			System.out.println(cardSuits[i]);*/
+
+		}	
+
+		/*for (Map.Entry<Integer,Integer
+		> entry : cardMap.entrySet()){
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+    	}*/
+	}
+
+	public void setCardMap(Integer i){
+		Integer initialValue = cardMap.get(i);
+
+		if (initialValue == null){
+			cardMap.put(i, 1);
+		}
+		else{
+			cardMap.put(i , initialValue+1);
+		}
+
+	}
+
+	public void setHighestCardValue(int i){
+
+		for (Integer cardValue : cardValueSet){
+			int highestCardValue = cardMap.get(cardValue);
+			/*System.out.println("Current card value: " + String.parseToString(cardValue) + );*/
+			if ( highestCardValue == i){
+				this.highestCardValue = highestCardValue;
 			}
 		}
-		
+
 	}
-	
-	public void setCardSuits(String[] cards) {
-		for (int i = 0; i < arrayLength; i++) {
-			String[] splitSuit = Arrays.stream(cards[i].split("\\d"))
-					.filter(s -> !s.isEmpty()).toArray(String[]::new);
-			
-			for (String suit: splitSuit) {
-				cardSuits[i] = suit;
-			}
-			
+	public void setCardValuesAndSuitsSet(){
+		cardValueSet = new TreeSet<>(Arrays.asList(cardValues));
+		cardSuitsSet = new TreeSet<>(Arrays.asList(cardSuits));
+
+		/*System.out.println(cardValueSet);
+		System.out.println(cardSuitsSet);
+		for (Integer i : cardValueSet){
+			System.out.println(i);
 		}
-		
+
+		for (String i : cardSuitsSet){
+			System.out.println(i);
+		}*/
 	}
 	
-	public boolean isHighCard() {
-		
+	
+	
+	public void isHighCard() {
+		setHighestCardValue(1);
 	}
+	
 	
 	public boolean isOnePair() {
+		Integer sizeCardValueSet = cardValueSet.size();
+
+		if ( sizeCardValueSet == 4){
+			setHighestCardValue(2);
+			return true;
+		}
+		else{
+			return false;
+		}
 		
 	}
 	
+	
 	public boolean isTwoPair() {
+		Integer sizeCardValueSet = cardValueSet.size();
+
+		if ( sizeCardValueSet == 3){
+			setHighestCardValue(2);
+			return true;
+		}
+		else{
+			return false;
+		}
 		
 	}
 	
 	public boolean isThreeOfAKind() {
+		Integer numberOfSuits = cardSuitsSet.size();
+		Integer sizeCardValueSet = cardValueSet.size();
+
+		if ( sizeCardValueSet == 3 && numberOfSuits >= 3){
+			setHighestCardValue(3);
+			return true;
+		}
+		else{
+			return false;
+		}
 		
 	}
 	
 	public boolean isStraight() {
+		Integer firstCard = cardValueSet.first();
+		Integer lastCard = cardValueSet.last();
+		Integer cardValueDifference = lastCard - firstCard;
+		Integer numberOfSuits = cardSuitsSet.size();
+		Integer sizeCardValueSet = cardValueSet.size();
+
+		if ( sizeCardValueSet == 5 && cardValueDifference ==  sizeCardValueSet-1 && numberOfSuits > 1){
+			setHighestCardValue(1);
+			return true;
+		}
+		else{
+			return false;
+		}
 		
 	}
 	
+	
 	public boolean isFlush() {
+		Integer numberOfSuits = cardSuitsSet.size();
+
+		if (numberOfSuits == 1){
+			setHighestCardValue(1);
+			return true;
+		}
+		else{
+			return false;
+		}
 		
 	}
+	
 	public boolean isFullHouse() {
+		Integer sizeCardValueSet = cardValueSet.size();
+
+		if ( sizeCardValueSet == 2 ){
+			setHighestCardValue(3);
+			return true;
+		}
+		else{
+			return false;
+		}
 		
 	}
 	
 	public boolean isFourOfAKind() {
+		Integer numberOfSuits = cardSuitsSet.size();
+		Integer sizeCardValueSet = cardValueSet.size();
+
+		if ( sizeCardValueSet == 2 && numberOfSuits == 4){
+			setHighestCardValue(4);
+			return true;
+		}
+		else{
+			return false;
+		}
 		
 	}
 	
 	public boolean isStraightFlush() {
+		Integer firstCard = cardValueSet.first();
+		Integer lastCard = cardValueSet.last();
+		Integer cardValueDifference = lastCard - firstCard;
+		Integer numberOfSuits = cardSuitsSet.size();
+		Integer sizeCardValueSet = cardValueSet.size();
+
+		if ( sizeCardValueSet == 5 && cardValueDifference ==  sizeCardValueSet -1 && numberOfSuits == 1){
+			setHighestCardValue(1);
+			return true;
+		}
+		else{
+			return false;
+		}
 		
 	}
 	
 	public boolean isRoyalFlush() {
+
+		Integer firstCard = cardValueSet.iterator().next();
+		Integer numberOfSuits = cardSuitsSet.size();
+
+		if ( firstCard == 10 && numberOfSuits ==1){
+			return true;
+		}
+		else{
+			return false;
+		}
 	
 	}
-		
+
+	public void currentHand(){
+		if (isRoyalFlush()){
+			System.out.println("Royal Flush");
+		}
+		else if (isStraightFlush()){
+			System.out.println("Straight Flush");
+		}
+		else if (isFourOfAKind()){
+			System.out.println("Four of a Kind");
+		}
+		else if (isFullHouse()){
+			System.out.println("Full House");
+		}
+		else if (isFlush()){
+			System.out.println("Flush");
+		}
+		else if (isStraight()){
+			System.out.println("Straight");
+		}
+		else if (isThreeOfAKind()){
+			System.out.println("Three of A Kind");
+		}
+		else if (isTwoPair()){
+			System.out.println("Two Pair");
+		}
+		else if (isOnePair()){
+			System.out.println("One Pair");
+		}
+		else{
+			System.out.println("High Card");
+		}
+
+		System.out.println(highestCardValue);
+	}
+	
 		
 }
